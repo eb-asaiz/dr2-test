426a194a1aa166dd711d8cbaa458c4f916c60583
eb-asaiz <asaiz@eventbrite.com>
2020-10-22 14:59:33 -0300

    add tests

--- drf2/api/serializers.py
+++ drf2/api/serializers.py
@@ -3,9 +3,5 @@ from rest_framework import serializers
 
 
 class SnippetSerializer(serializers.Serializer):
-    pk = serializers.Field()  # Note: `Field` is an untyped read-only field.
-    title = serializers.CharField(required=False,
-                                  max_length=100)
-    code = serializers.CharField(widget=widgets.Textarea,
-                                 max_length=100000)
-    linenos = serializers.BooleanField(required=False)
+    code = serializers.CharField(widget=widgets.Textarea, max_length=100000, required=True)
+    title = serializers.CharField(required=False, max_length=100)
